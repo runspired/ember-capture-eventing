288ddd539799c3f08f7917f85ef037490d6868e8
Chris Thoburn <runspired@gmail.com>
2015-08-20 18:24:29 -0500

    fix polyfill

--- addon/element-event-listener-list-polyfill.js
+++ addon/element-event-listener-list-polyfill.js
@@ -1,54 +1,68 @@
 /*global self, Element, window, document*/
-var addEventListener = self.addEventListener;
-var removeEventListener = self.removeEventListener;
+(function() {
+  var addEventListener = self.addEventListener;
+  var removeEventListener = self.removeEventListener;
 
-function EventListenerList() {}
-EventListenerList.prototype = [];
-EventListenerList.prototype.constructor = EventListenerList;
+  function EventListenerList() {}
+  EventListenerList.prototype = [];
+  EventListenerList.prototype.constructor = EventListenerList;
 
-function _nonEnumerable(obj, prop, initialValue) {
-  Object.defineProperty(
-    obj,
-    prop,
-    {
-      writable: true,
-      configurable: true,
-      enumerable: false,
-      value: initialValue
-    });
-}
+  _nonEnumerable(self, 'eventListenerList', {});
+  self.addEventListener = modifiedAddEventListener;
+  self.removeEventListener = modifiedRemoveEventListener;
+  if (typeof document !== "undefined") {
+    _nonEnumerable(document, 'eventListenerList', {});
+    _nonEnumerable(document.body, 'eventListenerList', {});
+    _nonEnumerable(Element.prototype, 'eventListenerList', {});
 
-_nonEnumerable(self, 'eventListenerList', {});
-if (typeof document !== "undefined") {
-  _nonEnumerable(document, 'eventListenerList', {});
-  _nonEnumerable(document.body, 'eventListenerList', {});
-  _nonEnumerable(Element.prototype, 'eventListenerList', {});
-}
+    document.addEventListener = modifiedAddEventListener;
+    document.removeEventListener = modifiedRemoveEventListener;
 
-function modifiedAddEventListener(event, handler, useCapture) {
-  this.eventListenerList[event] = this.eventListenerList[event] || [];
-  this.eventListenerList[event].push({
-    handler: handler,
-    name: handler.name || 'anonymous',
-    useCapture: useCapture || false
-  });
-  addEventListener.call(this, event, handler, useCapture);
-}
+    document.body.addEventListener = modifiedAddEventListener;
+    document.body.removeEventListener = modifiedRemoveEventListener;
 
-function modifiedRemoveEventListener(event, handler, useCapture) {
-  if (this.eventListenerList[event]) {
-    removeHandlerFromList(this.eventListenerList[event], handler, useCapture)
+    Element.prototype.addEventListener = modifiedAddEventListener;
+    Element.prototype.removeEventListener = modifiedRemoveEventListener;
   }
-  removeEventListener.call(this, event, handler, useCapture);
-}
 
-function removeHandlerFromList(list, handler, useCapture) {
-  useCapture = !!useCapture;
-  for (var i = 0; i < list.length; i++) {
-    var item = list[i];
-    if (item.handler === handler && item.useCapture === useCapture) {
-      list.splice(i, 1);
-      break;
+  function _nonEnumerable(obj, prop, initialValue) {
+    Object.defineProperty(
+      obj,
+      prop,
+      {
+        writable: true,
+        configurable: true,
+        enumerable: false,
+        value: initialValue
+      });
+  }
+
+  function modifiedAddEventListener(event, handler, useCapture) {
+    this.eventListenerList[event] = this.eventListenerList[event] || [];
+    this.eventListenerList[event].push({
+      handler: handler,
+      name: handler.name || 'anonymous',
+      useCapture: useCapture || false
+    });
+    addEventListener.call(this, event, handler, useCapture);
+  }
+
+  function modifiedRemoveEventListener(event, handler, useCapture) {
+    if (this.eventListenerList[event]) {
+      removeHandlerFromList(this.eventListenerList[event], handler, useCapture)
+    }
+    removeEventListener.call(this, event, handler, useCapture);
+  }
+
+  function removeHandlerFromList(list, handler, useCapture) {
+    useCapture = !!useCapture;
+    for (var i = 0; i < list.length; i++) {
+      var item = list[i];
+      if (item.handler === handler && item.useCapture === useCapture) {
+        list.splice(i, 1);
+        break;
+      }
     }
   }
-}
+
+})();
